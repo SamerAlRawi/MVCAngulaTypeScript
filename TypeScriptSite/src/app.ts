32545b21ab376fa0a64dc3a81974a4c0eef30f62
saa133 <samer.alrawi@sherwin.com>
2016-10-14 11:00:20 -0400

    init commit

---
+++ TypeScriptSite/src/app.ts
@@ -0,0 +1,18 @@
+﻿import {Component} from "angular2/core"
+import {MyModel} from "./model"
+
+//to enable prod mode in angular
+import {enableProdMode} from 'angular2/core';
+enableProdMode();
+
+@Component({
+    selector: `my-app`,
+    template: `<div>Hello from </div>`
+})
+
+export class MyApp {
+    model = new MyModel();
+    getCompiler() {
+        return this.model.compiler;
+    }
+}
